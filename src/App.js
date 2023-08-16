f354dc6a5915fbc8753cbb41f45a1d493d571c66
NitishSharma61 <sharmanitish616@gmial.com>
2023-08-16 16:40:28 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,328 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="index">
+      <div className="div">
+        <div className="component">
+          <div className="overlap-group">
+            <p className="lab-n-box">
+              <span className="text-wrapper">Lab</span>
+              <span className="span">&nbsp;</span>
+              <span className="text-wrapper">n</span>
+              <span className="span"> Box</span>
+            </p>
+            <p className="unleash-your-inner">
+              <span className="text-wrapper-2">Unleash Your</span>
+              <span className="text-wrapper-3">&nbsp;</span>
+              <span className="text-wrapper-4">
+                Inner Explorer!
+                <br />
+              </span>
+              <span className="text-wrapper-4">{""}</span>
+            </p>
+            <div className="group-wrapper">
+              <div className="group-2">
+                <p className="p">Join the Lab n box community</p>
+                <img
+                  className="vector"
+                  alt="Vector"
+                  src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/vector.svg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap">
+          <div className="group-3">
+            <div className="text-wrapper-5">Sign in</div>
+            <img
+              className="img"
+              alt="Vector"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/vector-3.svg"
+            />
+          </div>
+          <div className="group-4">
+            <div className="text-wrapper-6">Clan</div>
+            <img
+              className="vector-2"
+              alt="Vector"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/vector-4.svg"
+            />
+          </div>
+          <div className="group-5">
+            <div className="text-wrapper-7">My courses</div>
+            <img
+              className="group-6"
+              alt="Group"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/group-25@2x.png"
+            />
+          </div>
+          <div className="div-wrapper">
+            <div className="group-7">
+              <div className="text-wrapper-8">Store</div>
+              <img
+                className="vector-3"
+                alt="Vector"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/vector-5.svg"
+              />
+            </div>
+          </div>
+          <div className="group-8">
+            <div className="group-9">
+              <div className="text-wrapper-9">Home</div>
+              <img
+                className="vector-4"
+                alt="Vector"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/vector-6.svg"
+              />
+            </div>
+          </div>
+          
+        </div>
+        <div className="overlap-2">
+          <div className="group-10">
+            <div className="overlap-3">
+              <div className="text-wrapper-10">Start shopping</div>
+            </div>
+            <div className="vector-wrapper">
+              <img
+                className="vector-5"
+                alt="Vector"
+                src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/vector-2.svg"
+              />
+            </div>
+          </div>
+          <div className="group-11">
+            <div className="overlap-wrapper">
+              <div className="overlap-4">
+                <div className="overlap-wrapper">
+                  <div className="overlap-5">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-4">
+                        <div className="overlap-group-wrapper">
+                          <div className="overlap-group-2">
+                            <div className="rectangle" />
+                            <div className="text-wrapper-11">Webby DIY Chemistry Kit</div>
+                          </div>
+                        </div>
+                        <p className="text-wrapper-12">You save Rs. 1000 on launch offer</p>
+                      </div>
+                    </div>
+                    <img
+                      className="element"
+                      alt="Element"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/71yiexdoh7l-1@2x.png"
+                    />
+                  </div>
+                </div>
+                <div className="group-12">
+                  <div className="group-13">
+                    <div className="overlap-group-3">
+                      <div className="text-wrapper-13">Launch offer 50 %</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-14">
+                  <div className="text-wrapper-14">Rs. 1000</div>
+                  <div className="group-15">
+                    <div className="overlap-group-4">
+                      <div className="text-wrapper-15">Rs.2000</div>
+                      <img
+                        className="line"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/line-102.svg"
+                      />
+                      <img
+                        className="line"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/line-103.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="group-16">
+              <div className="overlap-4">
+                <div className="overlap-wrapper">
+                  <div className="overlap-5">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-4">
+                        <div className="overlap-group-wrapper">
+                          <div className="overlap-group-2">
+                            <div className="rectangle" />
+                            <div className="text-wrapper-11">Webby DIY Chemistry Kit</div>
+                          </div>
+                        </div>
+                        <p className="text-wrapper-12">You save Rs. 1000 on launch offer</p>
+                      </div>
+                    </div>
+                    <img
+                      className="element"
+                      alt="Element"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/71yiexdoh7l-1@2x.png"
+                    />
+                  </div>
+                </div>
+                <div className="group-12">
+                  <div className="group-13">
+                    <div className="overlap-group-3">
+                      <div className="text-wrapper-13">Launch offer 50 %</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-14">
+                  <div className="text-wrapper-14">Rs. 1000</div>
+                  <div className="group-15">
+                    <div className="overlap-group-4">
+                      <div className="text-wrapper-15">Rs.2000</div>
+                      <img
+                        className="line"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/line-102.svg"
+                      />
+                      <img
+                        className="line"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/line-103.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="group-17">
+              <div className="overlap-4">
+                <div className="overlap-wrapper">
+                  <div className="overlap-5">
+                    <div className="overlap-group-wrapper">
+                      <div className="overlap-4">
+                        <div className="overlap-group-wrapper">
+                          <div className="overlap-group-2">
+                            <div className="rectangle" />
+                            <div className="text-wrapper-11">Webby DIY Chemistry Kit</div>
+                          </div>
+                        </div>
+                        <p className="text-wrapper-12">You save Rs. 1000 on launch offer</p>
+                      </div>
+                    </div>
+                    <img
+                      className="element"
+                      alt="Element"
+                      src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/71yiexdoh7l-1@2x.png"
+                    />
+                  </div>
+                </div>
+                <div className="group-12">
+                  <div className="group-13">
+                    <div className="overlap-group-3">
+                      <div className="text-wrapper-13">Launch offer 50 %</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-14">
+                  <div className="text-wrapper-14">Rs. 1000</div>
+                  <div className="group-15">
+                    <div className="overlap-group-4">
+                      <div className="text-wrapper-15">Rs.2000</div>
+                      <img
+                        className="line"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/line-102.svg"
+                      />
+                      <img
+                        className="line"
+                        alt="Line"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/line-103.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p className="text-wrapper-16">Discover! Shop! Elevate Your Learning With One Time shop!</p>
+          <p className="your-gateway-to">
+            <span className="text-wrapper-17">Your Gateway To </span>
+            <span className="span">Knowledge And Innovation </span>
+          </p>
+        </div>
+        <div className="overlap-6">
+          <div className="img-wrapper">
+          </div>
+          <p className="access-boundless">
+            <span className="text-wrapper">Access Boundless Knowledge with</span>
+            <span className="span"> Subscription</span>
+          </p>
+          <div className="text-wrapper-18">Choose Your theme</div>
+          <p className="discover-the">
+            Discover the &#39;Fantastic, Remarkable&#39; Wonders - Monthly Delivery Awaits!
+          </p>
+          <div className="component-2">
+            <p className="text-wrapper-19">Receive Funfilled kit Every Month</p>
+            <div className="text-wrapper-20">3</div>
+          </div>
+          <div className="component-3">
+            <div className="text-wrapper-21">Choose Your Subscription</div>
+            <div className="text-wrapper-22">1</div>
+          </div>
+          <div className="component-4">
+            <div className="text-wrapper-23">Select Your Plan</div>
+            <div className="text-wrapper-20">2</div>
+          </div>
+          <div className="component-5">
+            <div className="text-wrapper-24">Chemistry kit</div>
+            <p className="chemistry-starter">
+              Chemistry starter kit for kids
+              <br />
+              Chemistry starter kit for kids !
+            </p>
+            <img
+              className="group-21"
+              alt="Group"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/group-363@2x.png"
+            />
+            <img
+              className="learn-climb-dynamo"
+              alt="Learn climb dynamo"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/learn-climb-dynamo-lab-science-set-24-piece-science-kit@2x.png"
+            />
+          </div>
+          <div className="component-6">
+            <p className="text-wrapper-25">Biology starter kit for kids ! Biology starter kit for kids !</p>
+            <div className="text-wrapper-26">Biology kit</div>
+            <img
+              className="group-21"
+              alt="Group"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/group-363@2x.png"
+            />
+            <img
+              className="element-pbnrnol"
+              alt="Element"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/81pbnr25nol-1@2x.png"
+            />
+          </div>
+          <div className="component-7">
+            <p className="text-wrapper-27">Steam starter kit for kids ! Steam starter kit for kids !</p>
+            <div className="text-wrapper-28">Steam kit</div>
+            <img
+              className="group-21"
+              alt="Group"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/group-363@2x.png"
+            />
+            <img
+              className="a-baa-b"
+              alt="A baa b"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64dc9c8c11684200ef7348b5/releases/64dc9cc4c298c6aa97ec7907/img/a2117121-b0aa-443b-ad15-a493aac99db1@2x.png"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
